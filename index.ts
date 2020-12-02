511df0ba9c542c64a18d19228920c9e194e7bdca
Ryan Pierce <rpierce@accurence.com>
2020-12-02 08:01:15 -0700

    Cleanup.

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 const isMultipleOf = (multiple: number, value: number) =>
     value % multiple === 0;
 
-const messageConfig: [number, string][] = [
-    [3, 'Fizz'],
-    [5, 'Buzz'],
-];
-
 interface MessagePartFn {
     (value: number): string;
 }
 
-const messageParts: MessagePartFn[] = messageConfig.map(
+const config: [number, string][] = [
+    [3, 'Fizz'],
+    [5, 'Buzz'],
+];
+
+const parts: MessagePartFn[] = config.map(
     ([multiple, message]) => (value) =>
         isMultipleOf(multiple, value) ? message : '',
 );
 
 const messageForValue = (value: number) =>
-    messageParts.reduce((message, part) => `${message}${part(value)}`, '');
+    parts.reduce((message, part) => `${message}${part(value)}`, '');
 
 const fizzBuzz = (value = 1, limit = 100) => {
     const message = messageForValue(value);
