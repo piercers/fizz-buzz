b7caf4f7494458e715cd68ef6657650c183633ea
Ryan Pierce <rpierce@accurence.com>
2020-12-02 03:03:43 -0700

    DRYer.

--- index.ts
+++ index.ts
@@ -4,14 +4,11 @@ const log = (index: number, message: string) =>
 const isMultipleOfX = (x: number, value: number) => value % x === 0;
 
 const fizzBuzz = (index = 1) => {
-    const multiple3 = isMultipleOfX(3, index);
-    const multiple5 = isMultipleOfX(5, index);
-    if (multiple3 && multiple5) {
-        log(index, 'FizzBuzz');
-    } else if (multiple3) {
-        log(index, 'Fizz');
-    } else if (multiple5) {
-        log(index, 'Buzz');
+    const fizz = isMultipleOfX(3, index) ? 'Fizz' : '';
+    const buzz = isMultipleOfX(5, index) ? 'Buzz' : '';
+    const message = `${fizz}${buzz}`;
+    if (message) {
+        log(index, message);
     }
     if (index < 100) {
         fizzBuzz(index + 1);
